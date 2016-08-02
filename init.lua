03a65fd7b452777ee2edf2f0f7d041dba8325eb8
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2016-08-02 16:15:06 +0200

    events and callbacks, polling all sensors on bus

--- init.lua
+++ init.lua
@@ -1,76 +1,143 @@
---tmr.alarm(0, 5e3, tmr.ALARM_SINGLE, search_sensors)
+-- filter(function, table)
+-- e.g: filter(is_even, {1,2,3,4}) -> {2,4}
+function filter(tbl, func)
+    local newtbl= {}
+    for i,v in pairs(tbl) do
+        if func(v) then
+            newtbl[i]=v
+        end
+    end
+    return newtbl
+end
 
 
-tmr.alarm(0, 5e3, tmr.ALARM_SINGLE, function() 
+local owpin = 5
+local owtmr = 5
+local addresses = {}
+local next_address = 1
 
-	pin = 5
-	ow.setup(pin)
-	count = 0
-	repeat
-		count = count + 1
-		addr = ow.reset_search(pin)
-		addr = ow.search(pin)
-		tmr.wdclr()
-	until (addr ~= nil) or (count > 100)
+function hexstr(str)
+	local result = ""
+	for i = 1, #str do
+		result = result .. string.format("%02X", str:byte(i))
+	end
+	return result
+end
 
-	if addr == nil then
-		print("no more addresses.")
-	else
-		print(addr:byte(1,8))
-		crc = ow.crc8(string.sub(addr,1,7))
+function search_addresses()
+	ow.setup(owpin)
+	ow.reset_search(owpin)
+
+	local result = {}
+
+	while true do
+		local addr = ow.search(owpin)
+		if addr == nil then
+			-- nothing more to find
+			break
+		end
+
+		local crc = ow.crc8(string.sub(addr, 1, 7))
+
 		if crc == addr:byte(8) then
-			if (addr:byte(1) == 0x10) or (addr:byte(1) == 0x28) then
-				print("device is a ds18S20 family device.")
-				repeat
-					ow.reset(pin)
-
-					ow.select(pin, addr)
-					ow.write(pin, 0x44, 1)
-					
-					-- t_CONV = 0.75s
-					-- delay of 1.0s ~ 50% failure
-					tmr.delay(1.5e6) -- increase to give measurement more time
-
-					present = ow.reset(pin)
-
-					ow.select(pin, addr)
-					ow.write(pin, 0xBE, 1)
-					
-					-- print("P="..present)
-
-					data = nil
-
-					data = string.char(ow.read(pin))
-					for i = 1,8 do
-						data = data .. string.char(ow.read(pin))
-					end
-
-					-- print(data:byte(1,9))
-
-					crc = ow.crc8(string.sub(data, 1, 8))
-					-- print("CRC=" .. crc)
-
-					if crc == data:byte(9) then
-						-- low byte first
-						local raw = (data:byte(1) + data:byte(2) * 256)
-						-- print(data:byte(1,2))
-						if raw == 0x00AA then
-							print("Temperature reads as power-on default value. Give it more time?")
-						else
-							-- 0x000 ~ 0 degrees celsius
-							local t = raw * 0.5
-							print("Temperature=" .. string.format("%.1f", t) .. " Centigrade")
-						end
-					end
-
-					tmr.wdclr()
-				until false
-			else
-				print("Device family is not recognized.")
-			end
+			table.insert(result, addr)
 		else
-			print("CRC is not valid!")
+			print("Invalid CRC: " .. hexstr(addr))
 		end
 	end
-end)
 
+	return result
+end
+
+function is_DS18S20(addr)
+	-- 64 bit string
+	-- check family code (8 bit)
+	return (addr:byte(1) == 0x10) -- or (addr:byte(1) == 0x28)
+	-- next 48 bit are serial number
+	-- upper 8 bit is CRC
+end
+
+function fetch_temp_reading(addr, callback)
+	local present = ow.reset(owpin) -- should not return anything
+	-- print("present: " .. present) -- returns 1 if a device is there
+
+	ow.select(owpin, addr)
+	ow.write(owpin, 0xBE, 1) -- Read Scratchpad command (returns 9 bytes)
+	
+	local data = ow.read_bytes(owpin, 9)
+
+	local crc = ow.crc8(string.sub(data, 1, 8))
+	-- print("CRC=" .. crc)
+
+	if crc ~= data:byte(9) then
+		print("CRC doesn't match!")
+		return callback(nil)
+	end
+
+	-- low byte first
+	local raw = (data:byte(1) + data:byte(2) * 256)
+	local t = raw * 0.5
+
+	-- print(data:byte(1,2))
+
+	if raw == 0x00AA then
+		print("Temperature reads as power-on default value. Give it more time?")
+	end
+
+	return callback(t)
+end
+
+
+function start_temp_reading(addr, callback)
+	-- print("Reading from addr " .. hexstr(addr))
+	ow.reset(owpin)
+	ow.select(owpin, addr) -- rom select
+	ow.write(owpin, 0x44, 1) -- Convert T command
+
+	-- t_CONV = 0.75s
+	-- delay of 1.0s ~ 50% failure
+	-- increase to give measurement more time
+	tmr.alarm(owtmr, 1.5e3, tmr.ALARM_SINGLE, function()
+		fetch_temp_reading(addr, callback)
+	end)
+
+	-- print("read initialized, waiting...?")
+end
+
+-- read_next = nil
+
+function read_next()
+	-- print("read next: index " .. next_address)
+
+	local addr = addresses[next_address]
+
+	start_temp_reading(addr, function(tempval)
+		print("Addr " .. hexstr(addr) .. " -> " .. string.format("%.1f", tempval) .. " Celsius")
+		tmr.alarm(owtmr, 2e3, tmr.ALARM_SINGLE, read_next)
+	end)
+
+	if next_address+1 <= #addresses then
+		next_address = next_address + 1
+	else
+		next_address = 1
+	end
+end
+
+tmr.alarm(0, 5e3, tmr.ALARM_SINGLE, function()
+
+	addresses = search_addresses()
+
+	print("Found Addresses:")
+	for i,addr in pairs(addresses) do
+		print("  " .. hexstr(addr))
+	end
+
+	addresses = filter(addresses, is_DS18S20)
+
+	print("Is DS18S20:")
+	for i,addr in pairs(addresses) do
+		print("  " .. hexstr(addr))
+	end
+
+	read_next()
+end)
