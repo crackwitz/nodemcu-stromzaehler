47dfe0490b43030c89b6d7438ad2151c4b0a2d97
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2016-09-11 23:11:57 +0200

    might not drop period power data, if there's just no absolute energy value set

---
+++ defs.lua
@@ -0,0 +1,170 @@
+-- definitions only, safe to dofile()
+
+function mqtt_onmessage(client, topic, message)
+	--print("received: " .. topic .. " -> " .. (message or "(nil)"))
+
+	if topic == basetopic .. "/restart" then
+		node.restart()
+
+	--	elseif topic == "electricity/energy" then
+	--		if not wattmeter.is_absolute and message ~= nil then
+	--			wattmeter:set_energy(tonumber(message))
+	--		end
+
+	elseif topic == "electricity/energy/set" then
+		if message ~= nil then
+			wattmeter:set_energy(tonumber(message))
+		end
+
+	--	elseif topic == "electricity/pulses-per-kWh" then
+	--		if message ~= nil then
+	--			wattmeter:set_pulses_per_kwh(tonumber(message))
+	--		end
+
+	elseif topic == "electricity/max-kW" then
+		if message == nil then
+			wattmeter.max_kw = nil
+		else
+			wattmeter.max_kw = tonumber(message)
+		end
+
+	elseif not startswith(topic, basetopic) then
+		return
+
+	end
+end
+
+function wattmeter.pulse_cb(dt, energy, power, round_increments)
+	if mqtt_client ~= nil then
+		if dt ~= nil then
+			mqtt_client:publish(
+				string.format("electricity/pulse_dt"),
+				string.format("%.6f", dt),
+				0, 0)
+		end
+		if energy ~= nil then
+			mqtt_client:publish(
+				string.format("electricity/energy"),
+				string.format("%.3f", energy),
+				0, 1) -- retain
+		end
+
+		if power ~= nil then
+			mqtt_client:publish(
+				string.format("electricity/power"),
+				string.format("%.3f", power),
+				0, 0)
+		end
+
+		if round_increments ~= nil then
+			for inc,dt in pairs(round_increments) do
+				mqtt_client:publish(
+					string.format("electricity/power/%skWh", inc),
+					string.format("%.4f", inc * 3600 / dt),
+					0, 0)
+			end
+		end
+	end
+end
+
+function wattmeter.period_cb(period, energy_max, power_min, power_max, power_mean)
+	print(string.format("period: E %.3f, P %.3f/%.3f/%.3f [W]", 
+		energy_max, power_min, power_mean, power_max
+	))
+
+	if graphite ~= nil then
+		if energy_max ~= nil then
+			graphite:send("electricity.energy", energy_max, period)
+		end
+		if power_min ~= nil then
+			graphite:send("electricity.power.min", power_min, period)
+		end
+		if power_max ~= nil then
+			graphite:send("electricity.power.max", power_max, period)
+		end
+		if power_mean ~= nil then
+			graphite:send("electricity.power", power_mean, period)
+		end
+	end
+
+	if mqtt_client ~= nil then
+		if power_min ~= nil then
+			mqtt_client:publish("electricity/power/min", string.format("%.3f", power_min), 0, 0)
+		end
+		if power_max ~= nil then
+			mqtt_client:publish("electricity/power/max", string.format("%.3f", power_max), 0, 0)
+		end
+		if power_mean ~= nil then
+			mqtt_client:publish("electricity/power/mean", string.format("%.3f", power_mean), 0, 0)
+			-- pulsecount * 3600 / 300 ~> 0.012 kW resolution
+		end
+	end
+end
+
+function mqtt_init()
+	mqtt_client = mqtt.Client(nodename, 10, nil, nil)
+	
+	--print("initializing mqtt")
+	mqtt_client:lwt(basetopic .. "/status", "offline", 0, 1)
+	mqtt_client:on("message", mqtt_onmessage)
+	mqtt_client:on("connect", function(client)
+		--print("mqtt connected")
+		--print("Subscribing")
+		--mqtt_client:subscribe(basetopic .. "/#", 0) -- HAS TO COME FIRST, only first subscription receives retained messages
+		--mqtt_client:subscribe("runlevel", 0)
+		mqtt_client:subscribe {
+			["runlevel"] = 0,
+			[basetopic .. "/#"] = 0,
+			["electricity/#"] = 0,
+		}
+
+		mqtt_client:publish(basetopic .. "/status", "online", 0, 1)
+		mqtt_client:publish(basetopic .. "/ip", ip, 0, 1)
+		if mdns ~= nil then
+			mqtt_client:publish(basetopic .. "/mdns", nodename, 0, 1)
+		end
+	end)
+	mqtt_client:close()
+	mqtt_client:connect("mqtt.space.aachen.ccc.de", 1883, 0, 1) -- secure 0, autoreconnect 1
+end
+
+function wlan_gotip()
+	ip, mask, gateway = wifi.sta.getip()
+	--print(string.format("IP:      %s", ip))
+	--print(string.format("Mask:    %s", mask))
+	--print(string.format("Gateway: %s", gateway))
+
+	Telnet.start()
+
+	sntp.sync(
+		"ptbtime1.ptb.de",
+		function(secs, usecs, server)
+			--print("Time Sync", secs, usecs, server)
+			wattmeter:time_changed()
+			if mqtt_client ~= nil then
+				mqtt_client:publish(basetopic .. "/started", string.format("%d.%06d", secs, usecs), 0, 1)
+			end
+		end
+	)
+	graphite:connect()
+	mqtt_init()
+
+	if mdns ~= nil then
+		mdns.register(nodename, {
+			port=2323,
+			service="telnet",
+			description="Lua REPL",
+			hardware="NodeMCU",
+			location="Serverraum"
+		})
+	end
+end
+
+function wlan_init()
+	--print("initializing wifi...")
+	wifi.setmode(wifi.STATION)
+	wifi.sta.config("CCCAC_PSK_2.4GHz", "23cccac42")
+	wifi.sta.eventMonReg(wifi.STA_GOTIP, wlan_gotip)
+	wifi.sta.eventMonStart()
+end
+
--- wattmeter.lua
+++ wattmeter.lua
@@ -1,31 +1,36 @@
 require 'funcs'
 
+-- TODO: maintain pulse count, which has no absolute reference, to not have to drop periods on set, etc
+
 Wattmeter = {
 	pin = nil, -- for gpio.trig() on falling edge
 
-	is_absolute = false, -- becomes true if energy was set to something absolute
-	pulses_per_kwh = 1000, -- 1000 pulses per kWh
 	lastpulse = nil, -- tmr.now() in microsecs [0..2^31), wraps around every 2^31 microsecs (36 minutes)
-	energy = 0.0, -- kWh
-	last_round = { -- index = math.round(energy / increment)
-		  [10] = 0, -- timestamp
+	pulsecount = 0, -- always increasing by 1, never reset
+	pulsebase = nil, -- energy = count * increment + base
+	pulses_per_kwh = 1000 / 1.0, -- 1000 pulses per kWh
+	kwh_per_pulse = 1.0 / 1000, -- inverse of above, assuming that multiplication is easier to softfloat lib
+	is_absolute = false, -- becomes true if energy was set to something absolute (base not nil)
+	last_round = { -- index = pulsecount
+		  [10] = 0, -- value is unix timestamp
 		 [100] = 0,
 		[1000] = 0,
 	},
-	window = 10,
-	pulse_history = {}, -- window+1 values of microsecond timestamps
 
 	max_kw = nil, -- kW, to debounce pulse rate, optional
 
 	-- uses rtctime for absolute periods (identified by centered timestamp)
-	period_interval = nil,
-	period_index = nil, -- index, absolute time % period
-	-- period indices are math.floor(timestamp / period_interval)
-	-- average power calculated from first pulse of new period (and dt)
+	-- evaluated on first pulse of new period (pulse belongs to new period)
+	-- only evaluate if all pulses during period were caught
+	period_interval = nil, -- config value
+
+	period_index = nil, -- index, math.floor(timestamp / period_interval)
+	-- on evaluation, if this is nil or not exactly 1 less than the new index, something's wrong and we don't report
+
+	period_firstcount = nil, -- set on proper rollover, refers to first pulse of previous period
+
 	period_power_max = nil, -- updated continuously
 	period_power_min = nil, -- updated continuously
-	period_energy_max = nil, -- updated continuously, refers to last pulse of previous period
-	period_energy_min = nil, -- updated at rollover, refers to first pulse of previous period
 
 	-- callbacks per pulse and per period
 	pulse_cb = nil, -- dt, energy, power, power_decimated
@@ -33,10 +38,26 @@ Wattmeter = {
 }
 Wattmeter.__index = Wattmeter
 
+local function nonnil_binop(binop, a, b)
+	if a == nil then
+		return b
+	elseif b == nil then
+		return a
+	else
+		return binop(a, b)
+	end
+end
+
+local function tmrdiff(t0, t1)
+	local delta = (t1 - t0) % 2^31
+	return delta * 1e-6
+end
+
 function Wattmeter:new(obj)
 	obj = obj or {}   -- create object if user does not provide one
 	setmetatable(obj, self)
 
+	obj:set_pulses_per_kwh(obj.pulses_per_kwh) -- reinit kwh_per_pulse
 	obj:install_gpio_trigger()
 
 	return obj
@@ -59,19 +80,22 @@ function Wattmeter:install_gpio_trigger(newpin)
 	end
 end
 
+function Wattmeter:set_pulses_per_kwh(pulserate)
+	self.pulses_per_kwh = pulserate
+	self.kwh_per_pulse = 1 / pulserate
+end
 
-function Wattmeter:get_increment() -- [kWh]
-	return 1 / self.pulses_per_kwh
+function Wattmeter:get_energy()
+	if self.is_absolute then
+		return self.pulsecount * self.kwh_per_pulse + self.pulsebase
+	else
+		return nil
+	end
 end
 
 function Wattmeter:set_energy(energy)
-	self.energy = energy
+	self.pulsebase = energy - self.pulsecount * self.kwh_per_pulse
 	self.is_absolute = true
-	self.period_energy_min = nil
-	self.period_index = nil
-	for k,v in pairs(self.last_round) do
-		self.last_round[k] = 0
-	end
 end
 
 local function rtc_now()
@@ -79,8 +103,20 @@ local function rtc_now()
 	return now + unow * 1e-6
 end
 
+function Wattmeter:time_changed()
+	-- reset period
+	self.period_index = nil -- self:get_period_index()
+
+	-- invalidate period, so nothing takes this as a reference
+	self.period_firstcount = nil -- this is important, will be set on next rollover
+
+	for k,v in pairs(self.last_round) do
+		self.last_round[k] = 0 -- 0 = unset (nil not possible, or else key disappears)
+	end
+end
+
 function Wattmeter:get_period_index()
-	if self.period_interval == nil then
+	if self.period_interval == nil then -- needs to be configured, or else nothing happens
 		return nil
 	end
 
@@ -91,19 +127,16 @@ function Wattmeter:get_period_index()
 	return math.floor(now / self.period_interval)
 end
 
-function Wattmeter:time_changed()
-	-- reset period
-	self.period_index = self:get_period_index()
-
-	-- invalidate period, so nothing takes this as a reference
-	self.period_energy_min = nil -- this is important, will be set on next rollover
-
-	for k,v in pairs(self.last_round) do
-		self.last_round[k] = 0
-	end
-end
-
 function Wattmeter:period_rollover(newindex)
+	-- rollover valid?
+	-- valid: period index incremented by 1 exactly
+	-- invalid: something was nil or we missed something
+
+	-- valid rollover -> period valid too?
+	-- valid: only if at previous valid rollover, period_firstcount was set (or else is nil)
+
+	-- TODO: newindex valid and is_absolute -> can report energy_max of previous index
+
 	local valid_rollover = nil
 	if newindex == nil or self.period_index == nil or newindex - self.period_index ~= 1 then
 		print(string.format("WARNING: period rollover from %d to %d (delta %+d)",
@@ -116,19 +149,25 @@ function Wattmeter:period_rollover(newindex)
 	end
 
 	-- period is valid?
-	if self.period_energy_min ~= nil then
-		local dE = self.energy - self.period_energy_min
-		-- self.energy will become period_energy_min of current period (see below)
+	if self.period_firstcount ~= nil then
+		local dEi = self.pulsecount - self.period_firstcount
+		-- more precisely, (pulsecount-1) - (firstcount-1), but -1 cancels out
+		local dE = dEi * self.kwh_per_pulse
 
 		local period_center = nil
 		if self.period_index ~= nil and self.period_interval ~= nil then
 			period_center = (self.period_index + 0.5) * self.period_interval
 		end
 
+		local energy_max = nil
+		if self.is_absolute then
+			energy_max = self.get_energy() - self.kwh_per_pulse -- subtract one pulse
+		end
+
 		if period_center ~= nil then
 			self.period_cb(
-				period_center, -- [seconds]
-				self.is_absolute and self.period_energy_max or nil, -- [kWh]
+				period_center, -- time [seconds]
+				energy_max, -- highest meter value in period (= previous pulse) [kWh]
 				self.period_power_min, -- [kW]
 				self.period_power_max, -- [kW]
 				dE * 3600 / self.period_interval -- [kW]
@@ -137,9 +176,9 @@ function Wattmeter:period_rollover(newindex)
 	end
 
 	if valid_rollover then
-		self.period_energy_min = self.energy
+		self.period_firstcount = self.pulsecount
 	else
-		self.period_energy_min = nil
+		self.period_firstcount = nil
 	end
 
 	self.period_index = newindex
@@ -147,26 +186,9 @@ function Wattmeter:period_rollover(newindex)
 	self.period_power_max = nil
 end
 
-local function nonnil_binop(binop, a, b)
-	if a == nil then
-		return b
-	elseif b == nil then
-		return a
-	else
-		return binop(a, b)
-	end
-end
-
-local function tmrdiff(t0, t1)
-	local delta = (t1 - t0) % 2^31
-	return delta * 1e-6
-end
-
 function Wattmeter:on_pulse()
 	local tmrnow = tmr.now() -- [us]
 	local now = rtc_now()
-
-	local increment = self:get_increment() -- [kWh]
 
 	-- estimate power using dt
 	-- use dt to last pulse (if any)
@@ -174,34 +196,28 @@ function Wattmeter:on_pulse()
 	local power = nil
 	if self.lastpulse ~= nil then
 		dt = tmrdiff(self.lastpulse, tmrnow) -- [seconds]
-		power = increment * 3600 / dt
+		power = self.kwh_per_pulse * 3600 / dt -- [kWs / s]
 
 		if self.max_kw ~= nil and power > self.max_kw then
-			-- ignore pulse
+			-- ignore pulse, could be bounced signal
 			return
 		end
 	end
 
 	-- add current pulse to accumulator
-	self.energy = self.energy + increment
+	self.pulsecount = self.pulsecount + 1
 	self.lastpulse = tmrnow
 
 	-- update round increments
-	local pulseindex = round(self.energy / increment)
 	local round_increments = {}
 	for k,v in pairs(self.last_round) do
-		if pulseindex % k == 0 then
+		if self.pulsecount % k == 0 then
 			if v > 0 then
-				round_increments[k * increment] = now - v
+				round_increments[k * self.kwh_per_pulse] = now - v
 			end
 			self.last_round[k] = now
 		end
 	end
-
-	-- update stats for period
-	self.period_energy_max = self.energy
-	self.period_power_min = nonnil_binop(math.min, self.period_power_min, power)
-	self.period_power_max = nonnil_binop(math.max, self.period_power_max, power)
 
 	-- rollover of period?
 	local new_period_index = self:get_period_index()
@@ -209,22 +225,16 @@ function Wattmeter:on_pulse()
 		self:period_rollover(new_period_index)
 	end
 
+	-- update stats for period
+	self.period_power_min = nonnil_binop(math.min, self.period_power_min, power)
+	self.period_power_max = nonnil_binop(math.max, self.period_power_max, power)
+
 	-- invoke per-pulse callback
 	if self.pulse_cb ~= nil then
-		-- compute power over window
-		local power_windowed = nil
-		local windowlen = #self.pulse_history
-		if windowlen >= self.window+1 then
-			local dE = (windowlen - 1) * increment
-			local dt = tmrdiff(self.pulse_history[1], self.pulse_history[windowlen])
-			power_windowed = dE * 3600 / dt
-			self.pulse_history = { self.pulse_history[windowlen] }
-		end
-
 		-- per-pulse callback
 		self.pulse_cb(
 			dt,
-			self.is_absolute and self.energy or nil, -- only report if we have a reference
+			self:get_energy(), -- report nil if no valid reference
 			power,
 			round_increments
 		)
